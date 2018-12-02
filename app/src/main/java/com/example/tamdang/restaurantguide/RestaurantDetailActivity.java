ea2767ac5c44ada3b79117f83be5c21d55127056
Tam Dang <danhtam0509@gmail.com>
2018-12-02 15:15:00 -0500

    set onClick event

--- app/src/main/java/com/example/tamdang/restaurantguide/RestaurantDetailActivity.java
+++ app/src/main/java/com/example/tamdang/restaurantguide/RestaurantDetailActivity.java
@@ -1,13 +1,29 @@
 package com.example.tamdang.restaurantguide;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RestaurantDetailActivity extends AppCompatActivity {
+
+    Button btnEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_detail);
+
+        //Defining Button
+        btnEdit = findViewById(R.id.btnEdit);
+
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), EditActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
