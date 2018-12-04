08a46998417d5e5be124e1c493407ea680844b08
Hao Bui <iamhaobui@gmail.com>
2018-12-03 19:54:25 -0500

    create Restaurant object class

--- app/src/main/java/com/example/tamdang/restaurantguide/Restaurant.java
+++ app/src/main/java/com/example/tamdang/restaurantguide/Restaurant.java
@@ -1,4 +1,59 @@
 package com.example.tamdang.restaurantguide;
 
 public class Restaurant {
+    private String name;
+    private String address;
+    private String phone;
+    private String description;
+    private String tag;
+
+    public Restaurant(String name, String address, String phone, String description, String tag) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.description = description;
+        this.tag = tag;
+    }
+
+    public String getAddress() {
+        return address;
+
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
