65d4dbfc753e7c0370b4890947186395facc9a1e
Hao Bui <iamhaobui@gmail.com>
2018-12-04 12:01:04 -0500

    modified EditActivity for showing and getting new data

--- app/src/main/java/com/example/tamdang/restaurantguide/EditActivity.java
+++ app/src/main/java/com/example/tamdang/restaurantguide/EditActivity.java
@@ -1,13 +1,73 @@
 package com.example.tamdang.restaurantguide;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class EditActivity extends AppCompatActivity {
+
+    private int position;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
+        Intent i = getIntent();
+
+        position = i.getIntExtra("position", -1);
+        String name = i.getStringExtra("name");
+        String address = i.getStringExtra("address");
+        String description = i.getStringExtra("description");
+        String phone = i.getStringExtra("phone");
+        String tag = i.getStringExtra("tag");
+
+        EditText edtName = findViewById(R.id.edtName);
+        EditText edtAddress = findViewById(R.id.edtAddress);
+        EditText edtPhone = findViewById(R.id.edtPhone);
+        EditText edtDescription = findViewById(R.id.edtDescription);
+        EditText edtTag = findViewById(R.id.edtTag);
+
+        edtName.setText(name);
+        edtAddress.setText(address);
+        edtPhone.setText(phone);
+        edtDescription.setText(description);
+        edtTag.setText(tag);
+
+        Button btnUpdate = findViewById(R.id.btnUpdate);
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent i = new Intent(v.getContext(), RestaurantActivity.class);
+                Intent i = getIntent();
+
+                EditText edtName1 = findViewById(R.id.edtName);
+                EditText edtAddress1 = findViewById(R.id.edtAddress);
+                EditText edtPhone1 = findViewById(R.id.edtPhone);
+                EditText edtDescription1 = findViewById(R.id.edtDescription);
+                EditText edtTag1 = findViewById(R.id.edtTag);
+
+                String name = edtName1.getText().toString();
+                String address = edtAddress1.getText().toString();
+                String phone = edtPhone1.getText().toString();
+                String description = edtDescription1.getText().toString();
+                String tag = edtTag1.getText().toString();
+
+                i.putExtra("position", position);
+                i.putExtra("name", name);
+                i.putExtra("address", address);
+                i.putExtra("phone", phone);
+                i.putExtra("description", description);
+                i.putExtra("tag", tag);
+
+                setResult(RESULT_OK, i);
+                finish();
+//                startActivityForResult(i, RESULT_OK);
+            }
+        });
     }
 }
