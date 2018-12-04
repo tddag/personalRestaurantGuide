ad2277ee06aef4ff682e2d51dd47c635bb0fc13c
Hao Bui <iamhaobui@gmail.com>
2018-12-04 12:00:11 -0500

    add remove item and onItemClick

--- app/src/main/java/com/example/tamdang/restaurantguide/RestaurantActivity.java
+++ app/src/main/java/com/example/tamdang/restaurantguide/RestaurantActivity.java
@@ -1,10 +1,13 @@
 package com.example.tamdang.restaurantguide;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -18,7 +21,7 @@ public class RestaurantActivity extends AppCompatActivity {
     private RestaurantAdapter restaurantsAdapter;
     private ListView lvRestaurants;
     public static final int ADD_RESTAURANT = 1;
-
+    public static final int DETAIL_RESTAURANT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +30,7 @@ public class RestaurantActivity extends AppCompatActivity {
 
         lvRestaurants = findViewById(R.id.restaurantsList);
         restaurants = new ArrayList<>();
-        restaurantsAdapter = new RestaurantAdapter(this, R.layout.activity_restaurant_detail, restaurants);
+        restaurantsAdapter = new RestaurantAdapter(this, R.layout.custom_layout, restaurants);
         lvRestaurants.setAdapter(restaurantsAdapter);
 
         // Defining Button
@@ -39,6 +42,36 @@ public class RestaurantActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i = new Intent(v.getContext(), AddActivity.class);
                 startActivityForResult(i, ADD_RESTAURANT);
+            }
+        });
+        lvRestaurants.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                final int pos = position;
+                new AlertDialog.Builder(view.getContext()).setTitle("Warning!")
+                        .setMessage("Do you want to remove this restaurant ?")
+                        .setNegativeButton(android.R.string.no, null)
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                restaurants.remove(pos);
+                                restaurantsAdapter.notifyDataSetChanged();
+                            }
+                        }).show();
+                return true;
+            }
+        });
+        lvRestaurants.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(view.getContext(), RestaurantDetailActivity.class);
+                i.putExtra("position", position);
+                i.putExtra("name", restaurants.get(position).getName());
+                i.putExtra("address", restaurants.get(position).getAddress());
+                i.putExtra("phone", restaurants.get(position).getPhone());
+                i.putExtra("description", restaurants.get(position).getDescription());
+                i.putExtra("tag", restaurants.get(position).getTag());
+                startActivityForResult(i, DETAIL_RESTAURANT);
             }
         });
     }
